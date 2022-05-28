3521a2eafb4599cf2d7f8d2c0357a50d3b0ef073
JBritoAvilez <98851943+JBritoAvilez@users.noreply.github.com>
2022-05-28 13:47:43 -0500

    Create Taller_1_M2.py

---
+++ Taller_1_M2/Taller_1_M2.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+a=1
+b=1
+c=1
+v=1
+d=2
+e=1
+f=1
+
+Algoritmo = (a + (b/v))/(d+(e/f))
+
+algoritmo = (a-(b/(c-d)))
+print(Algoritmo)
+print(algoritmo)
+
+ALgoritmo = Algoritmo
+Algoritmo = algoritmo
+algoritmo = ALgoritmo
+
+print(algoritmo)
+print(Algoritmo)
+
